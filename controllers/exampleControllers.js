437b130149d53e4a69ffea771239b5fdeaabf31e
dhomanhurimtm <dhomanhuri.ilyas@mtm.id>
2024-02-15 09:37:18 +0700

    add controller example

---
+++ controllers/exampleControllers.js
@@ -0,0 +1,18 @@
+const model = require("../models/index");
+
+const example = (req, res) => {
+    try {
+        let data = model.Todo.findAll();
+        if (!data) {
+            throw "data kosong";
+        }
+        res.status(200).send({
+            data: data,
+            message: "success",
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = { example };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const exampleControllers = require("../controllers/exampleControllers");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send("hhe");
+router.get("/", function (req, res, next) {
+    res.send("hhe");
 });
+
+router.get("/example", exampleControllers.example);
 
 module.exports = router;
